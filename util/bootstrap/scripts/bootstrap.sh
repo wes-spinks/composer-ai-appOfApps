93e4d2f962e58a8557a6e73510ad084dd4329132
Jamie Land <hokie10@gmail.com>
2024-10-08 16:38:26 -0400

    Adding startup scripts

--- util/bootstrap/scripts/bootstrap.sh
+++ util/bootstrap/scripts/bootstrap.sh
@@ -1,6 +1,8 @@
+echo "Bootstraping namespace..."
+
 # Create Weaviate Secret if it does not exist
 if oc get secret weaviate-api-key-secret >/dev/null 2>&1; then
-  echo "Secret 'weaviate-api-key-secret' already exists
+  echo "Secret 'weaviate-api-key-secret' already exists"
 else
   # Create the secret
   oc create secret generic weaviate-api-key-secret \
