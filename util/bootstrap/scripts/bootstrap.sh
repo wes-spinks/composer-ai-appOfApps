09807d12cdd4225281a64b0ff86ffca1f4f2fe05
Jamie Land <hokie10@gmail.com>
2024-10-08 20:12:42 -0400

    Updating to provide default client id

--- util/bootstrap/scripts/bootstrap.sh
+++ util/bootstrap/scripts/bootstrap.sh
@@ -6,6 +6,6 @@ if oc get secret weaviate-api-key-secret >/dev/null 2>&1; then
 else
   # Create the secret
   oc create secret generic weaviate-api-key-secret \
-    --from-literal=AUTHENTICATION_APIKEY_ALLOWED_KEYS="$(openssl rand -base64 32)"
+    --from-literal=AUTHENTICATION_APIKEY_ALLOWED_KEYS="$(head -c 32 /dev/urandom | base64)"
   echo "Secret 'weaviate-api-key-secret' created"
 fi
